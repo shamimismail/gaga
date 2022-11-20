9449f50d97e21c6a2e469661201871caaaa6cf07
shamimismail <ismailhossain092216@gmail.com>
2022-11-20 14:24:05 +0600

    first commit

---
+++ Day-4/main.js
@@ -0,0 +1,31 @@
+console.log('hello world')
+// string
+console.log("hello" + " world")
+console.log('hello world'.length)
+console.log('hello world'[1])
+console.log('hello "world')
+
+//  Operatior
+console.log(2+2)
+console.log(3-1)
+console.log(2/2)
+
+// Modulus
+console.log(10%2)
+console.log(10%3)
+
+// Exponential operator
+console.log(2**3)
+console.log(3**3)
+
+// undefine
+let var1;
+console.log(typeof var1)
+
+// Null
+let var2 = null;
+console.log(var2)
+
+// when to use let or const
+
+
